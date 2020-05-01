560c95cb7ec09de461b1d3320576d7c37137cf9f
Grindelf <grindelf.perlomutrovij@gmail.com>
2020-05-01 16:13:38 +0300

    Defined functions: print_title():, daytime(), greetings(), print_instructions(), to_number() and get_number():; structure file.

--- user-input-1.py
+++ user-input-1.py
@@ -1,62 +1,87 @@
 import math
 from datetime import datetime
 
-# Next lines are of the greetings:
 
-print("               FANTASTIC CALCULATOR")
-print("                 (or just FanC)")
-print('Welcome to the FANTASTIC CALCULATOR!')
-name = input("Please, stay calm and print your name: ")
+def print_title(version):
+    title_text = "\t\t\t\tFANTASTIC CALCULATOR\n" \
+                 "\t\t\t\t\t(or just FanC)\n" \
+                 "Welcome to the FANTASTIC CALCULATOR version " + str(version) + "!"
+    print(title_text)
 
-daytime = 'unknown daytime'
-hour_now = datetime.now().hour
 
-if 0 <= hour_now < 6:
-    daytime = 'night'
-elif 6 <= hour_now < 12:
-    daytime = 'morning'
-elif 12 <= hour_now < 18:
-    daytime = 'day'
-elif 18 <= hour_now <= 23:
-    daytime = 'evening'
-else:
-    daytime = ' - whatever there is at yours - '
+def daytime():
+    hour_now = datetime.now().hour
 
-hay = 'Good ' + daytime
+    if 0 <= hour_now < 6:
+        daytime_text = 'night'
+    elif 6 <= hour_now < 12:
+        daytime_text = 'morning'
+    elif 12 <= hour_now < 18:
+        daytime_text = 'day'
+    elif 18 <= hour_now <= 23:
+        daytime_text = 'evening'
+    else:
+        daytime_text = ' - whatever there is at yours - '
 
-print(hay + ', ' + name + '!')
-# Here ends the greetings. Next lines are description of calculating:
+    return daytime_text
 
-print("Let's do some Math! This calculator can do following operations:\n"
-      "+ -> addition\n"
-      "- -> subtraction\n"
-      "* -> multiplication\n"
-      "/ -> division\n"
-      "** -> exponentiation\n"
-      "V -> square root\n"
-      "% -> modulus\n"
-      "! -> factorial\n"
-      "log2x -> logarithm based on 2\n"
-      "lg -> logarithm based on 10\n"
-      "Please, if your number is decimal, use '.'")
-# Here ends description of calculating.
 
-# Next lines are of input:
+def greetings():
+    daytime_text = daytime()
+    name = input("Please, stay calm and print your name: ")
+    greetings_text = 'Good ' + daytime_text
+    return greetings_text + ", " + name + "!"
+
+
+def print_instructions():
+    instructions_text = "Let's do some Math! This calculator can do " \
+                        "following operations:\n" \
+                        "+ -> addition\n" \
+                        "- -> subtraction\n" \
+                        "* -> multiplication\n" \
+                        "/ -> division\n" \
+                        "** -> exponentiation\n" \
+                        "V -> square root\n" \
+                        "% -> modulus\n" \
+                        "! -> factorial\n" \
+                        "log2x -> logarithm based on 2\n" \
+                        "lg -> logarithm based on 10\n" \
+                        "Please, if your number is decimal, use '.'"
+    print(instructions_text)
+
+
+def to_number(number_as_text):
+    if "." in number_as_text:
+        number_as_digit = float(number_as_text)
+    else:
+        number_as_digit = int(number_as_text)
+
+    return number_as_digit
+
+
+def get_number(place_of_number):
+    number = input("Put here your " + place_of_number + " number -> |")
+    while number.count("-") > 1 or number.count(".") > 1 or \
+            not number.replace(".", "").replace("-", "").isdigit():
+        number = input("Are you sure, that you printed a number? "
+                       "Try again! Put here your first number -> |")
+
+    return to_number(number)
+
+
+# Here starts the program
+print_title("0.1")
+
+print(greetings())
+
+print_instructions()
+
 should_be_stopped = False
 
 operators = "+ - * / ** V % ! log2x lg"
 
 while not should_be_stopped:
-    first_number = input("Put here your first number -> |")
-    while first_number.count("-") > 1 or first_number.count(".") > 1 or \
-            not first_number.replace(".", "").replace("-", "").isdigit():
-        first_number = input("Are you sure, that you printed a number? "
-                             "Try again! Put here your first number -> |")
-    first_number_as_digit = 0
-    if "." in first_number:
-        first_number_as_digit = float(first_number)
-    else:
-        first_number_as_digit = int(first_number)
+    first_number_as_digit = get_number("first")
 
     operator = input("Choose one of this operators: " + operators + " -> |")
 
@@ -67,8 +92,8 @@ while not should_be_stopped:
                          "Try some of them again!"
                          "Choose one of this operators: " + operators + " -> |")
 
-    while first_number.count("-") > 0 and operator == "!" or first_number.count("-") > 0 and operator == "V" or \
-            first_number.count("-") > 0 and operator == "log2x" or first_number.count("-") > 0 and operator == "lg":
+    while first_number_as_digit < 0 and operator == "!" or first_number_as_digit < 0 and operator == "V" or \
+            first_number_as_digit < 0 and operator == "log2x" or first_number_as_digit < 0 and operator == "lg":
         first_number = input("It is impossible to do this operation with negative numbers. "
                              "Put here a positive one -> |")
         if "." in first_number:
@@ -77,17 +102,7 @@ while not should_be_stopped:
             first_number_as_digit = int(first_number)
 
     if operator != "!" and operator != "V" and operator != "log2x" and operator != "lg":
-        second_number = input("Put here your second number -> |")
-        while second_number.count("-") > 1 or second_number.count(".") > 1 or \
-                not second_number.replace(".", "").replace("-", "").isdigit():
-            second_number = input("Are you sure, that you printed a number? "
-                                  "Try again! Put here your first number -> |")
-
-        second_number_as_digit = 0
-        if "." in second_number:
-            second_number_as_digit = float(second_number)
-        else:
-            second_number_as_digit = int(second_number)
+        second_number = get_number("second")
     # Here ends the input.
 
     # Next lines are of the MATH:
@@ -121,4 +136,5 @@ while not should_be_stopped:
     x = input("Do you want to continue calculating? (Y/N): ")
     if x == "N":
         should_be_stopped = True
+
 # Here ends exit opportunity. It is the end of the code.
