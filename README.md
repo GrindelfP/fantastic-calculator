545b014c54713d9b35c1b4c87232c6977ded174e
GrindelfP <grindelf.perlomutrovij@gmail.com>
2020-07-04 12:49:49 +0300

    renew README.md, optimize code

--- README.md
+++ README.md
@@ -14,29 +14,35 @@
 
 2) "calculator.py" contains: 
 
-    a) libraries of operators, used in the calculator
+    a) libraries of operators and regexes, used in the calculator
      
     b) functions of calculating and checking the mathematical possibility 
-of performing certain operations.
+of performing certain operations
+
+    c) class Operator, containing all the operators and theirs specifications
 
 ## How to use the Calculator
 When You start the calculator it - 
-1) welcomes You and asks you about Your name to greet You by Your name
-2) explains, what operations You can do
+1) welcomes You and asks You about Your name to greet You by Your name
+2) explains, what operations and with what operators You can do
 3) asks You to print Your first number, operator and (in some cases) second
 number of Your calculation
+
+##### Important: when You want radical(root), logarithm or exponentiation You should type Your operator and instead of "i" print there index You want.
 4) (optional) shows You Your mistakes and asks to correct them
 5) prints You the result of Your calculation
-6) asks if You want to exit the Calculator or to continue calculating.
+6) asks if You want to exit the Calculator or continue calculating.
 
 ## Calculator's functional
 Fantastic Calculator can do following operations: 
 1) addition 
 2) subtraction 
 3) multiplication 
-4) division,
-5) exponentiation 
-6) modulo 
-7) square root 
+4) division
+5) exponentiation of power "i" 
+6) modulus 
+7) radical based on natural number "i" (root with a natural index) 
 8) factorial  
-9) logarithms based on 2 and 10.
+9) logarithms based on natural number "i"
+10) natural logarithms
+11) logarithms based on 10
--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 import calculator
-# Next lines are of functions, used in user-input-1:
 
 
-def title(version):
+def print_title(version):
     title_text = "\t\t\t\tFANTASTIC CALCULATOR\n" \
                  "\t\t\t\t\t(or just FanC)\n" \
                  "Welcome to the FANTASTIC CALCULATOR version " + str(version) + "!"
-    return title_text
+    return print(title_text)
 
 
 def daytime():
@@ -27,7 +26,7 @@ def daytime():
     return daytime_text
 
 
-def greetings():
+def print_greetings():
     daytime_text = daytime()
     name = input("Please, stay calm and print your name: ")
     if not name.strip():
@@ -48,6 +47,7 @@ def print_instructions():
     print(instructions_text_ending)
 
 
+# Next lines are of functions, used in user_interface:
 def to_number(number_as_text):
     if "." in number_as_text:
         number_as_digit = float(number_as_text)
@@ -55,14 +55,6 @@ def to_number(number_as_text):
         number_as_digit = int(number_as_text)
 
     return number_as_digit
-
-
-def variable_count():
-    variable_count_number = input("How much variables will have your operation? (Print a natural digit!) -> |")
-    while not variable_count_number.isdigit():
-        variable_count_number = input("You must enter number of operations as an natural integer! Try again! -> |")
-
-    return variable_count_number
 
 
 def get_number_input(original_prompt):
@@ -99,13 +91,13 @@ def exit_prompt():
         print("Good bye!")
 
     return to_be_stopped
+# Here ends lines of functions, used in user_interface. In next lines starts the program:
 
 
-# Here ends lines of functions, used in user-input-1. In next lines starts the program:
 # Here begins the greetings:
-print(title("1.4.1"))
+print_title("2.0.0")
 
-print(greetings())
+print_greetings()
 
 print_instructions()
 # Here ends the greetings. Next lines are of the input:
@@ -124,9 +116,9 @@ while not should_be_stopped:
 
 # Here ends the input. Next lines are of the calculating:
 
-    print("Your result is -> ", str(calculator.calculate(first_number_as_digit, user_operator, second_number_as_digit)))
+    print("Your result is -> ", str(calculator.calculate(first_number_as_digit, user_operator,
+                                                         second_number_as_digit)))
 
 # Here ends the calculating. Next lines are of the exit opportunity:
-
     should_be_stopped = exit_prompt()
 # Here ends the exit opportunity. Here ends the program.
